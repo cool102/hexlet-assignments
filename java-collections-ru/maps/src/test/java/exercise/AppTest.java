5a939f8dc13e92f502b60fcebb032fb9e8769ea9
@hexlet/cli <support@hexlet.io>
2022-10-05 14:00:31 +0300

    submit java-collections-ru/maps

--- java-collections-ru/maps/src/test/java/exercise/AppTest.java
+++ java-collections-ru/maps/src/test/java/exercise/AppTest.java
@@ -16,8 +16,8 @@ class AppTest {
         final int expectedText = 1;
         final int expectedDog = 1;
         String sentence1 = "word text dog apple word apple word";
-        Map actual1 = App.getWordCount(sentence1);
-        Map expected1 = new HashMap();
+        Map<String, Integer> actual1 = App.getWordCount(sentence1);
+        Map<String, Integer> expected1 = new HashMap<>();
         expected1.put("word", expectedWord);
         expected1.put("apple", expectedApple);
         expected1.put("text", expectedText);
@@ -25,7 +25,7 @@ class AppTest {
         assertThat(actual1).containsExactlyInAnyOrderEntriesOf(expected1);
 
         String sentence2 = "";
-        Map actual2 = App.getWordCount(sentence2);
+        Map<String, Integer> actual2 = App.getWordCount(sentence2);
         assertThat(actual2).isEmpty();
 
     }
@@ -35,21 +35,21 @@ class AppTest {
         String sentence1 = "word text cat apple word map apple word";
         final int intExpectedSize1 = 52;
         final int intExpectedSize2 = 43;
-        Map wordCount1 = App.getWordCount(sentence1);
+        Map<String, Integer> wordCount1 = App.getWordCount(sentence1);
         String actual1 = App.toString(wordCount1).trim();
         String[] expectedSubstrings1 = {"  apple: 2\n", "  cat: 1\n", "  text: 1\n", "  word: 3\n", "  map: 1\n"};
         assertThat(actual1).contains(expectedSubstrings1);
         assertThat(actual1).hasSize(intExpectedSize1);
 
         String sentence2 = "word text cat apple word apple word";
-        Map wordCount2 = App.getWordCount(sentence2);
+        Map<String, Integer> wordCount2 = App.getWordCount(sentence2);
         String actual2 = App.toString(wordCount2).trim();
         String[] expectedSubstrings2 = {"  apple: 2\n", "  cat: 1\n", "  text: 1\n", "  word: 3\n"};
         assertThat(actual2).contains(expectedSubstrings2);
         assertThat(actual2).hasSize(intExpectedSize2);
 
         String sentence3 = "";
-        Map wordCount3 = App.getWordCount(sentence3);
+        Map<String, Integer> wordCount3 = App.getWordCount(sentence3);
         String actual3 = App.toString(wordCount3);
         assertThat(actual3.trim()).isEqualTo("{}");
     }
