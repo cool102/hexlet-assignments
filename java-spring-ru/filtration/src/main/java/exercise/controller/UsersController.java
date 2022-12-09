c4a5a1ca4df457efa9c29a4df909f20554155ef0
@hexlet/cli <support@hexlet.io>
2022-12-09 07:29:20 +0300

    submit java-spring-ru/filtration

---
+++ java-spring-ru/filtration/src/main/java/exercise/controller/UsersController.java
@@ -0,0 +1,51 @@
+package exercise.controller;
+
+import exercise.model.User;
+import exercise.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+// Зависимости для самостоятельной работы
+// import org.springframework.data.querydsl.binding.QuerydslPredicate;
+// import com.querydsl.core.types.Predicate;
+
+@RestController
+@RequestMapping("/users")
+public class UsersController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    // BEGIN
+    @GetMapping("")
+    public Iterable<User> getUsersByFilter(@RequestParam(value = "firstName", required = false) String firstName,
+                                           @RequestParam(value = "lastName", required = false) String lastName) {
+        if (firstName == null && lastName == null) {
+            return userRepository.findAll();
+        }
+
+        if (firstName == null) {
+            return userRepository.findAll(
+                    QUser.user.lastName.containsIgnoreCase(lastName)
+            );
+        }
+        if (lastName == null) {
+            return userRepository.findAll(
+                    QUser.user.firstName.containsIgnoreCase(firstName)
+            );
+        }
+        return userRepository.findAll(
+                QUser.user.firstName
+                        .containsIgnoreCase(firstName)
+                        .and(
+                                QUser.user.lastName
+                                        .containsIgnoreCase(lastName)
+                        )
+        );
+    }
+    // END
+}
+
