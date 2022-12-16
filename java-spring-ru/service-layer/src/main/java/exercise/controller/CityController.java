1b2dbc239c8053bea47c740e1d9a3eb168162575
@hexlet/cli <support@hexlet.io>
2022-12-16 14:17:41 +0400

    submit java-spring-ru/service-layer

---
+++ java-spring-ru/service-layer/src/main/java/exercise/controller/CityController.java
@@ -0,0 +1,30 @@
+package exercise.controller;
+import exercise.model.City;
+import exercise.repository.CityRepository;
+import exercise.service.WeatherService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Map;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@RestController
+public class CityController {
+
+    @Autowired
+    private CityRepository cityRepository;
+
+    @Autowired
+    private WeatherService weatherService;
+
+    // BEGIN
+    
+    // END
+}
+
